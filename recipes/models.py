22342cce5b12c527e298fbfc5b55ba3655e77779
victorskie <welsonvictorskie@gmail.com>
2026-03-19 11:28:15 +0100

    commit after implementing video 68 from the git repository

--- recipes/models.py
+++ recipes/models.py
@@ -1,29 +1,38 @@
-from django.db import models
-from django.conf import settings
-# Create your models here.
-from .validators import validate_unit_of_measure
-from .utils import number_str_to_float
 import pint
+from django.conf import settings
+from django.db import models
 from django.urls import reverse
+from .utils import number_str_to_float
+from .validators import validate_unit_of_measure
+"""
+- Global
+    - Ingredients
+    - Recipes
+- User
+    - Ingredients
+    - Recipes
+        - Ingredients
+        - Directions for Ingredients
+"""
 
 class Recipe(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
     directions = models.TextField(blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
     active = models.BooleanField(default=True)
 
-    # def get_absolute_url(self):
-    #     return reverse("recipes:detail", kwargs={"id": self.id})
-
     def get_absolute_url(self):
+        return reverse("recipes:detail", kwargs={"id": self.id})
+
+    def get_hx_url(self):
         return reverse("recipes:hx-detail", kwargs={"id": self.id})
 
     def get_edit_url(self):
         return reverse("recipes:update", kwargs={"id": self.id})
-    
+
     def get_ingredients_children(self):
         return self.recipeingredient_set.all()
 
@@ -31,32 +40,42 @@ class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
-    quantity = models.CharField(max_length=50)
-    quantity_as_float = models.FloatField(blank=True , null=True)
+    quantity = models.CharField(max_length=50)  # 1 1/4
+    quantity_as_float = models.FloatField(blank=True, null=True)
+    # pounds, lbs, oz, gram, etc
     unit = models.CharField(max_length=50, validators=[validate_unit_of_measure])
     directions = models.TextField(blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
     active = models.BooleanField(default=True)
 
     def get_absolute_url(self):
         return self.recipe.get_absolute_url()
 
+    def get_hx_edit_url(self):
+        kwargs = {
+            "parent_id": self.recipe.id,
+            "id": self.id
+        }
+        return reverse("recipes:hx-ingredient-detail", kwargs=kwargs)
 
     def convert_to_system(self, system="mks"):
         if self.quantity_as_float is None:
             return None
         ureg = pint.UnitRegistry(system=system)
         measurement = self.quantity_as_float * ureg[self.unit]
-        return measurement
-    
+        return measurement #.to_base_units()
+
     def as_mks(self):
+        # meter, kilogram, second
         measurement = self.convert_to_system(system='mks')
         return measurement.to_base_units()
-    
+
     def as_imperial(self):
+        # miles, pounds, seconds
         measurement = self.convert_to_system(system='imperial')
         return measurement.to_base_units()
+
 
     def save(self, *args, **kwargs):
         qty = self.quantity
@@ -67,5 +86,5 @@ class RecipeIngredient(models.Model):
             self.quantity_as_float = None
         super().save(*args, **kwargs)
 
-# class GlobalIngredient(models.Model):
+# class RecipeImage():
 #     recipe = models.ForeignKey(Recipe)
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render , get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Recipe, RecipeIngredient
-from .forms import RecipeForm, RecipeIngredientForm
-from django.forms.models import modelformset_factory
-from django.http import HttpResponse
+from django.forms.models import modelformset_factory # model form for querysets
 from django.urls import reverse
-# Create your views here.
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect, render, get_object_or_404
+
+from .forms import RecipeForm, RecipeIngredientForm
+from .models import Recipe, RecipeIngredient
+# CRUD -> Create Retrieve Update & Delete
 
 @login_required
 def recipe_list_view(request):
     qs = Recipe.objects.filter(user=request.user)
-    context ={ 
+    context = {
         "object_list": qs
     }
     return render(request, "recipes/list.html", context)
@@ -19,67 +20,89 @@ def recipe_list_view(request):
 @login_required
 def recipe_detail_view(request, id=None):
     hx_url = reverse("recipes:hx-detail", kwargs={"id": id})
-    obj = get_object_or_404(Recipe, id=id, user=request.user)
-    context ={ 
+    context = {
         "hx_url": hx_url
     }
-    return render(request, "recipes/detail.html", context)
+    return render(request, "recipes/detail.html", context) 
 
 
 @login_required
-def recipe_detail_hx_view(request, id):
+def recipe_detail_hx_view(request, id=None):
+    if not request.htmx:
+        raise Http404
     try:
         obj = Recipe.objects.get(id=id, user=request.user)
     except:
-        obj=None
-    if obj is None:
+        obj = None
+    if obj is  None:
         return HttpResponse("Not found.")
-    context ={ 
+    context = {
         "object": obj
     }
-    return render(request, "recipes/partials/detail.html", context)
-
-
-
+    return render(request, "recipes/partials/detail.html", context) 
 
 
 @login_required
 def recipe_create_view(request):
     form = RecipeForm(request.POST or None)
-    context ={ 
-        "form":form 
+    context = {
+        "form": form
     }
     if form.is_valid():
         obj = form.save(commit=False)
         obj.user = request.user
         obj.save()
         return redirect(obj.get_absolute_url())
-    return render(request, "recipes/create-update.html", context)
+    return render(request, "recipes/create-update.html", context)  
 
+@login_required
+def recipe_update_view(request, id=None):
+    obj = get_object_or_404(Recipe, id=id, user=request.user)
+    form = RecipeForm(request.POST or None, instance=obj)
+    new_ingredient_url = reverse("recipes:hx-ingredient-create", kwargs={"parent_id": obj.id})
+    context = {
+        "form": form,
+        "object": obj,
+        "new_ingredient_url": new_ingredient_url
+    }
+    if form.is_valid():
+        form.save()
+        context['message'] = 'Data saved.'
+    if request.htmx:
+        return render(request, "recipes/partials/forms.html", context)
+    return render(request, "recipes/create-update.html", context)  
 
 
 @login_required
-def recipe_update_view(request, id):
-    obj = get_object_or_404(Recipe, id=id, user=request.user)
-    form = RecipeForm(request.POST or None, instance=obj)
-    RecipeIngredientFormset = modelformset_factory(RecipeIngredient, form=RecipeIngredientForm, extra=0)
-    qs = obj.recipeingredient_set.all()
-    formset = RecipeIngredientFormset(request.POST or None, queryset=qs)
-
+def recipe_ingredient_update_hx_view(request, parent_id=None, id=None):
+    if not request.htmx:
+        raise Http404
+    try:
+        parent_obj = Recipe.objects.get(id=parent_id, user=request.user)
+    except:
+        parent_obj = None
+    if parent_obj is  None:
+        return HttpResponse("Not found.")
+    instance = None
+    if id is not None:
+        try:
+            instance = RecipeIngredient.objects.get(recipe=parent_obj, id=id)
+        except:
+            instance = None
+    form = RecipeIngredientForm(request.POST or None, instance=instance)
+    url = reverse("recipes:hx-ingredient-create", kwargs={"parent_id": parent_obj.id})
+    if instance:
+        url = instance.get_hx_edit_url()
     context = {
-        "form":form,
-        "formset":formset,
-        "object": obj,
+        "url": url,
+        "form": form,
+        "object": instance
     }
-
-    if all([ form.is_valid(), formset.is_valid() ]) :
-        parent = form.save(commit=False)
-        parent.save()
-        for form in formset: 
-            child = form.save(commit=False)
-            child.recipe = parent 
-            child.save()
-        context['message'] = 'Date saved'
-    if request.htmx:
-        return render(request, "recipes/partials/forms.html", context)
-    return render(request, "recipes/create-update.html", context)
+    if form.is_valid():
+        new_obj = form.save(commit=False)
+        if instance is None:
+            new_obj.recipe = parent_obj
+        new_obj.save()
+        context['object'] = new_obj
+        return render(request, "recipes/partials/ingredient-inline.html", context) 
+    return render(request, "recipes/partials/ingredient-form.html", context) 
