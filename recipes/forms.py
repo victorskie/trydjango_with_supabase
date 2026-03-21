ac6cba75a5a0717cc257339a705b644af262cf7f
victorskie <welsonvictorskie@gmail.com>
2026-03-21 09:30:52 +0100

    commit after implementing video 69 from the git repository

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,27 +1,37 @@
-from django import forms 
+from django import forms
+
+
 from .models import Recipe, RecipeIngredient
 
 class RecipeForm(forms.ModelForm):
     error_css_class = 'error-field'
     required_css_class = 'required-field'
-    name = forms.CharField(help_text='this is your help!  <a href="/contact">Contact us</a>' )
-    # description = forms.CharField(widget=forms.Textarea( attrs={"rows": "3"}))
+    name = forms.CharField(help_text='This is your help! <a href="/contact">Contact us</a>')
+    # descriptions = forms.CharField(widget=forms.Textarea(attrs={"rows": 3}))
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'directions']
-
-    def __init__(self,*args, **kwargs):
+    
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # django-crispy-forms
         for field in self.fields:
             new_data = {
-                "placeholder": f'Recipe {str(field)}', "class": 'form-control'
+                "placeholder": f'Recipe {str(field)}',
+                "class": 'form-control',
+                "hx-post": ".",
+                "hx-trigger": "keyup changed delay:500ms",
+                "hx-target": "#recipe-container",
+                "hx-swap": "outerHTML"
             }
             self.fields[str(field)].widget.attrs.update(
                 new_data
             )
-        self.fields['name'].label = 'Name'
-        self.fields['name'].widget.attrs.update({'class': 'form-control-2'})
-        self.fields['description'].widget.attrs.update({'rows': '4'})
+        # self.fields['name'].label = ''
+        # self.fields['name'].widget.attrs.update({'class': 'form-control-2'})
+        self.fields['description'].widget.attrs.update({'rows': '2'})
+        self.fields['directions'].widget.attrs.update({'rows': '4'})
+
 
 class RecipeIngredientForm(forms.ModelForm):
     class Meta:
