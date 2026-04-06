fb7906122fb0e0e340dbc4c5522fea9fc70099fa
victorskie <welsonvictorskie@gmail.com>
2026-04-06 09:29:54 +0100

    commit after changes from video 71

--- article/models.py
+++ article/models.py
@@ -1,85 +1,73 @@
-from django.db import models
-from django.utils import timezone
-from django.db.models.signals import pre_save, post_save
-from .utils import slugify_instance_title
-from django.urls import reverse
-from django.db.models import Q
 from django.conf import settings
-
+from django.db import models
+from django.db.models import Q
+from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
+from django.utils import timezone
 # Create your models here.
 
-
+from .utils import slugify_instance_title
 
 User = settings.AUTH_USER_MODEL
 
-
-class ArticleQueryset(models.QuerySet):
+class ArticleQuerySet(models.QuerySet):
     def search(self, query=None):
         if query is None or query == "":
-            return self.none() # []
+            return self.none()
         lookups = Q(title__icontains=query) | Q(content__icontains=query)
-        return self.filter(lookups)
+        return self.filter(lookups) 
 
 class ArticleManager(models.Manager):
     def get_queryset(self):
-        return ArticleQueryset(self.model, using=self._db)
-    
+        return ArticleQuerySet(self.model, using=self._db)
+
     def search(self, query=None):
         return self.get_queryset().search(query=query)
 
-
-# class ArticleManager(models.Manager):
-#     def search(self, query=None):
-#         if query is None or query == "":
-#             return self.get_queryset().none() # []
-#         lookups = Q(title__icontains=query) | Q(content__icontains=query)
-#         return self.get_queryset().filter(lookups)
-
-
-
-
 class Article(models.Model):
+    # https://docs.djangoproject.com/en/3.2/ref/models/fields/#model-field-types
+    # Django model-field-types
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
-    slug = models.SlugField(unique=True, blank=True, null=True) 
+    slug = models.SlugField(unique=True, blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     publish = models.DateField(auto_now_add=False, auto_now=False, null=True, blank=True)
+
     objects=ArticleManager()
 
+    @property
+    def name(self):
+        return self.title
+
     def get_absolute_url(self):
+        # return f'/articles/{self.slug}/'
         return reverse("articles:detail", kwargs={"slug": self.slug})
 
     def save(self, *args, **kwargs):
+        # obj = Article.objects.get(id=1)
+        # set something
         # if self.slug is None:
         #     self.slug = slugify(self.title)
+        # if self.slug is None:
+        #     slugify_instance_title(self, save=False)
         super().save(*args, **kwargs)
+        # obj.save()
+        # do another something
 
 
-
-
-
-
-
-
-def article_pre_save(sender, instance ,*args, **kwargs):
+def article_pre_save(sender, instance, *args, **kwargs):
     # print('pre_save')
     if instance.slug is None:
         slugify_instance_title(instance, save=False)
-    else:
-        # Case 2: Slug provided? Check it for uniqueness!
-        slugify_instance_title(instance, save=False, new_slug=instance.slug)
 
 pre_save.connect(article_pre_save, sender=Article)
 
 
-
-def article_post_save(sender, instance, created ,*args, **kwargs):
+def article_post_save(sender, instance, created, *args, **kwargs):
     # print('post_save')
     if created:
         slugify_instance_title(instance, save=True)
 
-        
 post_save.connect(article_post_save, sender=Article)
-
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for trydjango project.
+"""trydjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,30 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
-from django.urls import path, include
-
-from .views import home_view
-
-# from article.views import (
-#     article_create_view,
-#     article_detail_view,
-#     article_search_view,
-# )
+from django.urls import include, path, re_path
 
 from accounts.views import (
     login_view,
     logout_view,
-    register_view,
-
+    register_view
 )
 
+from search.views import search_view
+from .views import home_view
+
 urlpatterns = [
-    path('', home_view),
+    path('', home_view), # index / home / root 
     path('pantry/recipes/', include('recipes.urls')),
-    path('articles/', include('article.urls')),
-    path('login/', login_view),
+    path('articles/', include('articles.urls')),
+    path('search/', search_view, name='search'),
     path('admin/', admin.site.urls),
+    path('login/', login_view),
     path('logout/', logout_view),
-    path('register/', register_view)
+    path('register/', register_view),
 ]
